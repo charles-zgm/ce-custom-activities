a478efcded07922b3dc6ee53710bc349cb45d88b
Charles Robinson <>
2022-11-04 13:32:17 -0400

    update

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,1 +1,31 @@
-console.log("execute");
+var settings = {
+  "url": "https://www.carrierenterprise.com/rest/V1/customers/password",
+  "method": "PUT",
+  "timeout": 0,
+  "headers": {
+    "content-type": "application/json",
+    "authority": "www.carrierenterprise.com",
+    "pragma": "no-cache",
+    "cache-control": "no-cache",
+    "authorization": "",
+    "accept": "application/json",
+    "store": "default",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Safari/537.36",
+    "x-wso-job": "",
+    "x-wso-account": "",
+    "x-wso-branch": "",
+    "sec-gpc": "1",
+    "origin": "https://www.carrierenterprise.com",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-dest": "empty",
+    "referer": "https://www.carrierenterprise.com/forgot-password",
+    "accept-language": "en-US,en;q=0.9"
+  },
+  "data": JSON.stringify({
+    "email": "charles@zerogravitymarketing.com",
+    "template": "email_reset",
+    "websiteId": 1
+  }),
+};
+
